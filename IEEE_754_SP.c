1bab78190d5ae9d1948f99d384029bf2fcb40eec
mobro98 <45178670+mobro98@users.noreply.github.com>
2018-11-19 20:43:04 -0500

    Add files via upload

---
+++ IEEE_754_SP.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int binary_convertor(int a, int b);
+
+typedef union
+{
+    float number;
+    struct
+    {
+        unsigned int mantissa_sp : 23;
+        unsigned int exponent_sp : 8;
+        unsigned int sign_sp : 1;
+    }bits;
+}sN;
+
+int main()
+{
+    sN sn;
+    printf("************IEEE 754 Floating Point Single Precision Number Converter************\n");
+
+        while(1)
+        {
+            
+            printf("\nInput Float Number: ");
+            scanf("%f",&sn.number);
+            printf("\n");
+            printf("%d ",sn.bits.sign_sp);
+            binary_convertor(sn.bits.exponent_sp, 8);
+            printf(" ");
+            binary_convertor(sn.bits.mantissa_sp, 23);
+            printf("\n");
+        }
+    
+    
+    return 0;
+}
+
+int binary_convertor(int a, int b)
+{
+    int c;
+    for (b--; b >= 0; b--)
+    {
+        c = a >> b;
+        if (c & 1)
+            printf("1");
+        else
+            printf("0");
+    }
+    return 0;
+}
